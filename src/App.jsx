decac1c6f578dd59b50ad10ba71b75a55508ea3a
Nidhi Chaudhary <nivee.1401@gmail.com>
2025-02-10 15:25:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className='w-full h-screen'> 
+      <Navbar/>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     </div>
+  )
+}
+
+export default App
